2f69e5f2ae3357994773274d1f697567d8df1ed3
imajinyun <imajinyun@gmail.com>
2019-02-22 23:56:07 +0800

    Add helpers.php

--- app/Supports/helpers.php
+++ app/Supports/helpers.php
@@ -1,1 +1,13 @@
 <?php
+
+if (! function_exists('route_class')) {
+    /**
+     * 将路由名称转换为页面 div class 名称
+     *
+     * @return string
+     */
+    function route_class()
+    {
+        return str_replace('.', '-', \Illuminate\Support\Facades\Route::currentRouteName());
+    }
+}
