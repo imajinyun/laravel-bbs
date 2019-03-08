d398aedf3a1a489ff90f99efd2f43cf8519e3111
imajinyun <imajinyun@gmail.com>
2019-03-08 12:04:41 +0800

    Add user information filling

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -15,11 +15,16 @@ use Faker\Generator as Faker;
 */
 
 $factory->define(App\Models\User::class, function (Faker $faker) {
+    $datetime = $faker->dateTimeBetween('-2 years');
+
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        'introduction' => $faker->sentence(),
         'remember_token' => Str::random(10),
+        'created_at' => $datetime,
+        'updated_at' => $datetime,
     ];
 });
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,56 @@
+<?php
+
+use App\Models\User;
+use Faker\Generator;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        /** @var \Faker\Generator $faker */
+        $faker = app(Generator::class);
+
+        $avatars = [
+            'https://entities.oss-cn-beijing.aliyuncs.com/laravel/bbs/avatar/Awesome8dae1Coer.jpg',
+            'https://entities.oss-cn-beijing.aliyuncs.com/laravel/bbs/avatar/Aiqueeyov7aiFaiy.jpg',
+            'https://entities.oss-cn-beijing.aliyuncs.com/laravel/bbs/avatar/Cae8ro9Reequae2x.jpg',
+            'https://entities.oss-cn-beijing.aliyuncs.com/laravel/bbs/avatar/ChooDi9booDuo5uo.jpg',
+            'https://entities.oss-cn-beijing.aliyuncs.com/laravel/bbs/avatar/DeiH5uo6ooTahRat.jpg',
+            'https://entities.oss-cn-beijing.aliyuncs.com/laravel/bbs/avatar/Eewa6jezoQuoeghi.jpg',
+            'https://entities.oss-cn-beijing.aliyuncs.com/laravel/bbs/avatar/Eiyoo9ohthie9ahl.jpg',
+            'https://entities.oss-cn-beijing.aliyuncs.com/laravel/bbs/avatar/Ohr8ohtahdeefiem.jpg',
+            'https://entities.oss-cn-beijing.aliyuncs.com/laravel/bbs/avatar/Oo5ia7Aib3aaquoh.jpg',
+            'https://entities.oss-cn-beijing.aliyuncs.com/laravel/bbs/avatar/OobapaiV1eweeTh6.jpg',
+            'https://entities.oss-cn-beijing.aliyuncs.com/laravel/bbs/avatar/Oorooyealai9dobo.jpg',
+            'https://entities.oss-cn-beijing.aliyuncs.com/laravel/bbs/avatar/Peb3wuKuiPha1gei.jpg',
+            'https://entities.oss-cn-beijing.aliyuncs.com/laravel/bbs/avatar/Tooz8meeha6gooVu.jpg',
+            'https://entities.oss-cn-beijing.aliyuncs.com/laravel/bbs/avatar/aMagh1EefaNgi2ei.jpg',
+            'https://entities.oss-cn-beijing.aliyuncs.com/laravel/bbs/avatar/aecah3Eo1shahchu.jpg',
+            'https://entities.oss-cn-beijing.aliyuncs.com/laravel/bbs/avatar/ueCie3oufai7euco.jpg',
+        ];
+
+        /** @var \Illuminate\Database\Eloquent\FactoryBuilder $factory */
+        $factory = factory(User::class)
+            ->times(15)
+            ->make()
+            ->each(function ($user) use ($faker, $avatars) {
+                $user->avatar = $faker->randomElement($avatars);
+            });
+
+        // 让隐藏字段可见，并将数据集合转换为数组
+        $users = $factory->makeVisible(['password', 'remember_token'])->toArray();
+        User::insert($users);
+
+        $user = User::find(1);
+        $user->name = 'imajinyun';
+        $user->email = 'imajinyun@vip.qq.com';
+        $user->avatar = $avatars[0];
+        $user->save();
+    }
+}
