f678d58b4fab5badc96431e5455d4c23780bee24
imajinyun <imajinyun@gmail.com>
2019-03-11 23:02:06 +0800

    Generate failed jobs table migration

---
+++ database/migrations/2019_03_11_212806_create_failed_jobs_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFailedJobsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('failed_jobs', function (Blueprint $table) {
+            $table->bigIncrements('id')
+                ->comment('主键 ID');
+            $table->text('connection')
+                ->comment('连接');
+            $table->text('queue')
+                ->comment('队列');
+            $table->longText('payload')
+                ->comment('载荷');
+            $table->longText('exception')
+                ->comment('异常');
+            $table->timestamp('failed_at')
+                ->useCurrent()
+                ->comment('失败时间');
+        });
+
+        $query = "ALTER TABLE `failed_jobs` COMMENT '失败的作业表'";
+        DB::statement($query);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('failed_jobs');
+    }
+}
