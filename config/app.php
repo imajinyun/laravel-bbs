ddc8bf147d71bff175c5669e9d73d2ce41f41b55
imajinyun <imajinyun@gmail.com>
2019-03-12 16:07:56 +0800

    Add lang provider

--- config/app.php
+++ config/app.php
@@ -159,6 +159,7 @@ return [
         Illuminate\Auth\Passwords\PasswordResetServiceProvider::class,
         Illuminate\Session\SessionServiceProvider::class,
         Illuminate\Translation\TranslationServiceProvider::class,
+        Overtrue\LaravelLang\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
