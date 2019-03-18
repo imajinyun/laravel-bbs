28e773bee46735346dccc036085c3a4e601846f1
imajinyun <imajinyun@gmail.com>
2019-03-18 16:00:21 +0800

    Add upload file migration

---
+++ database/factories/FileGroupFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(\App\Models\FileGroups::class, function (Faker $faker) {
+    $datetime = now();
+
+    return [
+        'is_public' => 1,
+        'created_at' => $datetime,
+        'updated_at' => $datetime,
+    ];
+});
---
+++ database/seeds/FileGroupsSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Models\FileGroups;
+use Illuminate\Database\Seeder;
+
+class FileGroupsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        /** @var \Faker\Generator $faker */
+        $faker = app(\Faker\Generator::class);
+
+        $data = [
+            ['name' => '默认文件组', 'code' => 'default'],
+            ['name' => '缩略图片组', 'code' => 'thumb'],
+            ['name' => '用户头像组', 'code' => 'avatar'],
+            ['name' => '临时目录组', 'code' => 'tmp'],
+            ['name' => '全局设置组', 'code' => 'system'],
+            ['name' => '用户私有组', 'code' => 'private'],
+        ];
+        $factory = factory(FileGroups::class)
+            ->times(6)
+            ->make()
+            ->each(function ($fileGroup, $index) use ($data) {
+                $fileGroup->name = $data[$index]['name'];
+                $fileGroup->code = $data[$index]['code'];
+
+                if ($fileGroup->code === 'private') {
+                    $fileGroup->is_public = 0;
+                }
+            });
+        $fileGroups = $factory->toArray();
+        FileGroups::insert($fileGroups);
+    }
+}
