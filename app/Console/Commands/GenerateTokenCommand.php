6ce17bbf031072d2faa518b1ea07cc52f721a566
imajinyun <imajinyun@gmail.com>
2019-04-06 16:28:04 +0800

    Add generate user token command

---
+++ app/Console/Commands/GenerateTokenCommand.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Auth;
+
+class GenerateTokenCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'bbs:generate-token';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Generate token for user';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $userId = $this->ask('Please enter the user ID:');
+
+        if (! $user = User::find($userId)) {
+            return $this->error('User does not exist.');
+        }
+
+        $ttl = now()->addYear()->getTimestamp();
+        $this->info(Auth::guard('api')->setTTL($ttl)->fromUser($user));
+    }
+}
