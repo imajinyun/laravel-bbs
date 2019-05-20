979e3cf88e2d2c606ec65323bd5a459f22d77f33
imajinyun <imajinyun@gmail.com>
2019-05-20 11:20:45 +0800

    Update password length

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,7 +21,7 @@ $factory->define(App\Models\User::class, static function (Faker $faker) {
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        'password' => '$2y$10$J6BFBA/Fb/tZuAsXuYneL.MUKPWq04xchN9x.Y5koSAeqrHpNCHwi', // laraveler
         'introduction' => $faker->sentence(),
         'remember_token' => Str::random(10),
         'created_at' => $datetime,
