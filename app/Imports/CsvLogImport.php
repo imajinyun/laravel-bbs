827a71c64b84c358eff59c3f2c53aa3af571e172
imajinyun <imajinyun@gmail.com>
2022-03-21 21:27:13 +0800

    formatting

--- app/Imports/CsvLogImport.php
+++ app/Imports/CsvLogImport.php
@@ -12,10 +12,10 @@ use Maatwebsite\Excel\Concerns\WithHeadingRow;
 class CsvLogImport implements ToModel, WithHeadingRow, WithBatchInserts, WithChunkReading
 {
     /**
-    * @param array $row
-    *
-    * @return \Illuminate\Database\Eloquent\Model|null
-    */
+     * @param array $row
+     *
+     * @return \Illuminate\Database\Eloquent\Model|null
+     */
     public function model(array $row)
     {
         $row = array_filter(array_values($row));
